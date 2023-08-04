6300eb270bcf99e1d688b8caf4d2f5340bcb2b2e
codeblooded01 <vishnupms1998@gmail.com>
2023-08-04 17:04:59 +0530

    functionalities added

--- src/components/QuestionForm.jsx
+++ src/components/QuestionForm.jsx
@@ -27,7 +27,10 @@ import {
 } from "@mui/material";
 import { BsFile, BsTrash } from "react-icons/bs";
 import { FcRightUp } from "react-icons/fc";
+import { DragIndicator } from "@mui/icons-material";
+import {DragDropContext,Droppable,Draggable} from 'react-beautiful-dnd'
 import "./questionform.css";
+
 
 function QuestionForm() {
     const [questions, setQuestions] = useState([
@@ -40,6 +43,9 @@ function QuestionForm() {
           { optionText: "Hubbli" },
           { optionText: "Malesia" },
         ],
+        answer:false,
+        answerKey:"",
+        points:0,
         open: true,
         required: false,
       },
@@ -74,13 +80,122 @@ function QuestionForm() {
         console.log(i+'___' +j)
       }
     }
-  
+    function addOption(i){
+      let optionsOfQuestion = [...questions];
+      if(optionsOfQuestion[i].options.length <5){
+        optionsOfQuestion[i].options.push({optionText:"Option" + (optionsOfQuestion[i].options.length + 1)})
+      }
+      else{
+        console.log("Max 5 options")
+      }
+      setQuestions(optionsOfQuestion)
+    }
+    function copyQuestion(i){
+      expandCloseAll()
+      let qs = [...questions]
+      var newquestion = {...qs[i]}
+      setQuestions([...questions,newquestion])
+    }
+
+    function deleteQuestion(i){
+      let qs = [...questions]
+      if(questions.length > 1){
+        qs.splice(i,1)
+      }
+      setQuestions(qs)
+    }
+    function requiredQuestion(i){
+      let reqQuestion = [...questions]
+      reqQuestion[i].required = ! reqQuestion[i].required
+      console.log(reqQuestion[i].required+" "+i)
+      setQuestions(reqQuestion)
+    }
+    function addMoreQuestionField() {
+      expandCloseAll()
+      setQuestions([
+        ...questions,
+        {
+          questionText: "Question",
+          questionType: "radio",
+          options: [{ optionText: "Option 1" }],
+          open: true,
+          required: false,
+        }
+      ]);
+    }
+    function expandCloseAll(){
+      let qs = [...questions]
+      for(let j=0;j<qs.length;j++){
+        qs[j].open = false
+      }
+      setQuestions(qs)
+    }
+    function handleExpand(i){
+      let qs = [...questions]
+      for(let j =0;j<qs.length;j++){
+        if(i ===j){
+          qs[i].open =true
+        }
+        else{
+          qs[j].open = false
+        }
+      }
+      setQuestions(qs)
+    }
+    function setOptionAnswer(ans,qno){
+      let Questions = [...questions]
+      Questions[qno].answerKey = ans
+
+      setQuestions(Questions)
+      console.log(qno + " "+ans)
+    }
+
+    function setOptionPoints(points,qno){
+      let Questions = [...questions];
+      Questions[qno].points = points
+      setQuestions(Questions)
+      console.log(qno + " "+points)
+    }
+    function doneAnswer(i){
+      let answerOfQuestion = [...questions];
+      answerOfQuestion[i].answer = !answerOfQuestion[i].answer
+      setQuestions(answerOfQuestion)
+      
+    }
+    function addAnswer(i){
+      let answerOfQuestion = [...questions];
+      answerOfQuestion[i].answer = !answerOfQuestion[i].answer
+      setQuestions(answerOfQuestion)
+      
+    }
+    // function onDragEnd(result){
+    //   if(!result.destination){
+    //     return
+    //   }
+    //   var itemgg = [...questions]
+    //   const itemF = reorder(
+    //     itemgg,
+    //     result.source.index,
+    //     result.destination.index
+    //   )
+    //   setQuestions(itemF)
+    // }
+    // const reorder = (list,startIndex,endIndex)=>{
+    //   const result = Array.from(list)
+    //   const [removed] = result.splice(startIndex,1)
+    //   result.splice(endIndex,0,removed)
+    //   return result
+    // }
+    
+
+    
     function questionsUI() {
       return questions.map((ques, i) => (
         <Accordion
           key={i}
-          expanded={ques.open}
-          className={ques.open ? "add_border" : ""}
+          expanded={questions[i].open}
+          className={questions[i].open ? "add_border" : ""}
+          onChange={()=>{handleExpand(i)}}
         >
           <AccordionSummary
             aria-controls="panel1a-content"
@@ -88,7 +203,7 @@ function QuestionForm() {
             elevation={1}
             style={{ width: "100%" }}
           >
-            {questions.open ? (
+            {!questions[i].open ? (
                 <div className="saved_questions">
 
                 <Typography style={{fontSize:"15px",fontWeight:"400",letterSpacing:'.1px',lineHeight:'24px',paddingBottom:"8px"}} >
@@ -117,7 +232,9 @@ function QuestionForm() {
                 </div>
             ):""}
           </AccordionSummary>
+
           <div className="question_boxes">
+          {!questions[i].answer ? (
            <AccordionDetails className="add_question">
             <div className="add_question_top">
               <input type="text" className="question" placeholder="Question" value={ques.questionText} onChange={(e)=>{changeQuestion(e.target.value, i)}}/>
@@ -159,7 +276,7 @@ function QuestionForm() {
                         } label={
                           <div>
                             <input type="text" className="text_input" style={{fontSize:"13px",width:"60px"}} placeholder="Add other"/>
-                            <Button size="small" style={{fontSize:"13px",fontWeight:"600",textTransform:'none',color:"#4285f4"}}>Add Option</Button>
+                            <Button size="small" onClick={()=>{addOption(i)}} style={{fontSize:"13px",fontWeight:"600",textTransform:'none',color:"#4285f4"}}>Add Option</Button>
                           </div>
                         } />
                       </div>
@@ -168,19 +285,19 @@ function QuestionForm() {
 
                     <div className="add_footer">
                       <div className="add_question_botton_left">
-                        <Button size="small" style={{textTransform:"none",color:"#4285f4",fontSize:"13px",fontWeight:"600"}}>
+                        <Button size="small" style={{textTransform:"none",color:"#4285f4",fontSize:"13px",fontWeight:"600"}} onClick={()=>{addAnswer(i)}}>
                           <FcRightUp style={{border:"2px solid #4285f4",padding:"2px",marginRight:"8px"}}/>Answer key
                         </Button>
 
                       </div>
                       <div className="add_question_bottom">
-                        <IconButton aria-label="Copy">
+                        <IconButton aria-label="Copy" onClick={()=>{copyQuestion(i)}}>
                           <FilterNone />
                         </IconButton>
-                        <IconButton aria-label="delete">
+                        <IconButton aria-label="delete" onClick={()=>{deleteQuestion(i)}}>
                           <BsTrash />
                         </IconButton>
-                        <span style={{color:"#5f6368",fontSize:"13px"}}>Required</span><Switch name="checkedA" color="primary" checked />
+                        <span style={{color:"#5f6368",fontSize:"13px"}} onClick={()=>{requiredQuestion(i)}}>Required</span><Switch name="checkedA" color="primary" checked />
                         <IconButton >
                           <MoreVert />
                         </IconButton>
@@ -188,13 +305,62 @@ function QuestionForm() {
                       </div>
 
                     </div>
+           </AccordionDetails>):(
+           <AccordionDetails className="add_question">
+            <div className="top_header">
+              Choose correct answer
+            </div>
+            <div>
+              <div className="question_top">
+                <input type="text" className="question" placeholder="Question" value={ques.questionText} disabled/>
+                <input type="number" className="points" min="0" step="1" placeholder="0" onChange={(e)=>{setOptionPoints(e.target.value,i)}}/>
+              </div>
+              {ques.options.map((op,j)=>(
+                <div className="add_question_body" key={j} style={{marginLeft:"8px",marginBottom:"10px",marginTop:"5px"}}>
+                  <div key={j}>
+                    <div style={{display:"flex"}} className="">
+                      <div className="form_check">
+                        <label style={{fontSize:"13px"}} onClick={()=>{setOptionAnswer(ques.options[j].optionText,i)}}>
+                          {(ques.questionType !=="text") ?
+                          <input type={ques.questionType}
+                          name={ques.questionText}
+
+                          value="option3"
+                          className="form_check_input"
+                          required={ques.required}
+                          style={{marginRight:"10px",marginBottom:"10px",marginTop:"5px"}}
+                          />:<ShortText style={{marginLeft:"10px"}} />}
+
+                          {ques.options[j].optionText}
+                        </label>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+              ))}
+              <div className="add_question_body">
+                <Button size="small" style={{textTransform:"none",color:"#4285f4",fontSize:"13px",fontWeight:"600"}}>
+                  <BsFile style={{fontSize:"20px",marginRight:"8px"}}/>Add Answer Feedback
+                </Button>
+              </div>
+              <div className="add_question_bottom">
+              <Button variant="outlined" color="primary" size="small" style={{textTransform:"none",color:"#4285f4",fontSize:"12px",fontWeight:"600",marginTop:"12px"}}
+              onClick={()=>{doneAnswer(i)}}>Done</Button>
+              </div>
+            </div>
            </AccordionDetails>
-          <div className="question_edit">
-           <AddCircleOutline className="edit" />
+           )}
+
+        {!ques.answer ?(
+           <div className="question_edit">
+           <AddCircleOutline className="edit" onClick={addMoreQuestionField}/>
            <OndemandVideo className="edit" />
            <CropOriginal className="edit" />
            <TextFields className="edit" />
-          </div>
+          </div>):" "} 
 
           </div>
         </Accordion>
@@ -219,8 +385,21 @@ function QuestionForm() {
                 placeholder="Form description"
               />
             </div>
-            {questionsUI()}
           </div>
+          {/* <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable">
+              {(provided,snapshot)=>(
+                <div
+                {...provided.droppableProps}
+                ref = {provided.innerRef}
+                >
+                {provided.placeholder}
+                </div>
+                )}
+                </Droppable>
+              </DragDropContext> */}
+              {questionsUI()}
+        
         </div>
       </div>
     );
